6f159bf0163bb72617496b276a8ee21861d37be2
ANZAL12 <mohammedanzel123@gmail.com.com>
2026-03-06 13:55:20 +0530

    add auto superuser creation to build script

--- backend/build.sh
+++ backend/build.sh
@@ -6,4 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-    
+
+# Automatically create the superuser without needing the Render Shell
+DJANGO_SUPERUSER_PASSWORD="Anzal@2002" python manage.py createsuperuser --email mohammedanzel123@gmail.com --noinput || true
